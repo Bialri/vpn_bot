ed2c5eaaec1a79c746df5ffbb3c52255f201a8d6
Bialri <kardasov96@mail.ru>
2024-03-13 20:53:17 +0300

    feat(wireguard_server): message handler

---
+++ src/wireguard_server/consumer/schemas.py
@@ -0,0 +1,11 @@
+from pydantic import BaseModel
+from enum import Enum
+
+class StateEnum(Enum):
+    running = 'running'
+    stopped = 'stopped'
+
+class ChangeStateMessage(BaseModel):
+    interface: str
+    status: StateEnum
+    hostname: str
